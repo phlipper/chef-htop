8293d164faf4a43ef267fb998d5619934a1f0e2e
Phil Cohen <github@phlippers.net>
2013-11-06 10:58:33 -0800

    bump version to 1.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs htop"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.1.0"
+version          "1.2.0"
 
 recipe "htop", "Installs htop monitoring"
 
